1ba5cc1368af631adcbc531179075739bb0d546e
Guilherme Lopes <guilhermelopes@gmail.com>
2025-07-11 11:59:55 -0300

    meu codigo da conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var scanner = new Scanner(System.in); 
+        System.out.println("Por favor, me fale seu nome:");
+        var NomeCliente = scanner.next();
+        System.out.println("Agora o número da conta:");
+        var numero = scanner.nextInt(); 
+        System.out.println("Qual é a sua agência:");
+        var agencia = scanner.next();
+        System.out.println("quanto você quer deposita:");
+        var saldo = scanner.nextFloat();
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s já está disponível para saque", NomeCliente, agencia, numero, saldo);
     }
 }
